8ce99977416b3b4031dc7c988ca84b4f67a2ba86
mikolalex <laaazyyy@gmail.com>
2018-06-03 19:17:47 +0300

    todo example improved

--- spec/testComponents/Todo.js
+++ spec/testComponents/Todo.js
@@ -1,6 +1,46 @@
 import React from 'react';
 import { withMrr, skip } from './mrr';
-import _ from 'lodash';
+
+const styles = `
+
+body {
+    padding: 10px;
+}
+.options {
+    margin: 0;
+    padding: 0;
+    display: inline-block;
+}
+.todo-item {
+    border: 1px dotted grey;
+    padding: 10px;
+    margin: 10px 0;
+}
+.remove  {
+    float: right;
+}
+.remove, .clear-completed, .options li {
+    color: blue;
+    text-decoration: underline;
+    display: inline-block;
+    margin: 0 5px;
+    cursor: pointer;
+}
+.options li.active {
+    border: 1px dotted grey;
+    color: black;
+    padding: 3px;
+    text-decoration: none;
+}
+.text {
+    display: inline-block;
+    padding: 0 10px;
+}
+.todo-app > * {
+    margin: 20px 0;
+}
+
+  `;
 
 const prop = a => b => b[a];
 const id = a => a;
@@ -8,10 +48,7 @@ const not = a => !a;
 const always = a => () => a;
 const incr = a => a + 1;
 const asProp = prop => val => ({ [prop]: val })
-const assoc = prop => val => obj => ({
-  ...obj,
-  [prop]: val,
-})
+const assoc = prop => val => obj => Object.assign({}, obj, { [prop]: val });
 const I = (...funcs) => arg => {
   funcs.forEach(f => {
     arg = f(arg);
@@ -21,7 +58,7 @@ const I = (...funcs) => arg => {
 const ifPressed = key => e => e.key == key || skip;
 
 
-const todos = [
+const todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [
   {
     id: 1,
     text: 'Save the world',
@@ -52,20 +89,22 @@ const TodoItem = withMrr({
     'update': id,
   }, '-newText', 'finishEdit'],
   remove: [asProp('id'), '-id', ['merge', 'afterEdit.remove', 'removeItem']],
-}, (state, props, $) => <div>
-   <input type="checkbox" checked={ props.completed } onChange={ $('complete', !props.completed) } /> &nbsp;&nbsp;&nbsp; <a onClick={ $('removeItem') }>Remove</a>
+}, (state, props, $) => ( <div className="todo-item">
+   <input type="checkbox" checked={ props.completed } onChange={ $('complete', !props.completed) } />
+   <div className="remove">
+    <a onClick={ $('removeItem') }>Remove</a>
+   </div>
    { state.isEdited
-     ? <div><input defaultValue={ props.text } onChange={ $("newText") } onKeyPress={ $('finishEdit', ifPressed('Enter')) } /></div>
-     : <div onClick={ $("startEdit") }>{ props.text }</div>
+     ? <div className="text"><input defaultValue={ props.text } onChange={ $("newText") } onKeyPress={ $('finishEdit', ifPressed('Enter')) } /></div>
+     : <div className="text" onClick={ $("startEdit") }>{ props.text }</div>
    }
-   <br /><br />
-
- </div>);
+ </div>));
 
 const Todos = withMrr({
   $init: {
     todos,
     show: '*',
+    shownTodos: [],
   },
   newTodoItem: [I(asProp('text'), assoc('completed')(false)), '-newInput', 'addNewTodo'],
   newInput: [always(''), 'newTodoItem'],
@@ -88,38 +127,49 @@ const Todos = withMrr({
       [always({ completed: true}), 'clear_done']
     ],
   }],
-  openNumber: [todos => todos.filter(I(prop('completed'), not)).length, 'todos'],
-  shownTodos: [(a, val) => a.filter(i => i.completed !== val), 'todos', 'show'],
+  openNumber: [todos => todos.filter(I(prop('completed'), not)).length, 'todos', '$start'],
+  shownTodos: [(a, val) => a.filter(i => i.completed !== val), 'todos', 'show', '$start'],
+  $$toLS: [arr => localStorage.setItem('todos', JSON.stringify(arr)), 'todos'],
 }, (state, props, $, connectAs) => {
   return (
-    <div>
+    <div className="todo-app">
+      <style dangerouslySetInnerHTML={ { __html: styles } }></style>
       <div>
         <h1>Todos</h1>
         {
           state.shownTodos.map(todo => {
-              const props = { ...todo, ...connectAs(todo.id), key: todo.id };
+              const props = Object.assign({}, todo, connectAs(todo.id), { key: todo.id });
               return <TodoItem {...props} />;
           })
         }
-      </div><br />
-      Show: <br />
-      <ul>
-        <li onClick={ $('show', '*') } className={ (state.show === '*') ? 'active' : '' }>
-            All
-        </li>
-        <li onClick={ $('show', false) } className={ (state.show === false) ? 'active' : '' }>
-          Completed
-        </li>
-        <li onClick={ $('show', true) } className={ (state.show === true) ? 'active' : ''}>
-          Open
-        </li>
-      </ul><br /><br />
-      <a onClick={ $('clear_done') }>Remove completed</a><br /><br />
-      <input type="checkbox" value={ state.openNumber === 0 ? 'on' : 'off' } checked={ state.openNumber === 0 } onClick={ $('toggleCompleted') } />
-      Check/uncheck all<br /><br />
-      Add new: <input type="text" value={ state.newInput } onChange={ $('newInput') } onKeyPress={ $("addNewTodo", ifPressed('Enter')) } />
-      <br /><br />
-      { state.openNumber } items left
+      </div>
+	  <div>
+		<input placeholder="What needs to be done?" type="text" value={ state.newInput } onChange={ $('newInput') } onKeyPress={ $("addNewTodo", ifPressed('Enter')) } />
+	  </div>
+	  { (state.openNumber !== state.todos.length) && <div>
+		<a onClick={ $('clear_done') } className="clear-completed">Clear completed</a>
+	  </div> }
+	  { !!state.todos.length && <div>
+		<input type="checkbox" value={ state.openNumber === 0 ? 'on' : 'off' } checked={ state.openNumber === 0 } onClick={ $('toggleCompleted') } />
+		Check/uncheck all
+	  </div> }
+	  { !!state.todos.length && <div>
+		Show:
+		<ul className="options">
+		<li onClick={ $('show', '*') } className={ (state.show === '*') ? 'active' : '' }>
+			All
+		</li>
+		<li onClick={ $('show', false) } className={ (state.show === false) ? 'active' : '' }>
+		  Completed
+		</li>
+		<li onClick={ $('show', true) } className={ (state.show === true) ? 'active' : ''}>
+		  Open
+		</li>
+		</ul>
+	  </div> }
+	  { !!state.todos.length && <div>
+		{ state.openNumber } items left
+	  </div> }
     </div>);
 });
 
